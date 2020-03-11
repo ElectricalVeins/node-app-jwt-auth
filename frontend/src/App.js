fff622c2cd28e45c2f6075d2c235335c94f1a381
eveins <maxmost512@gmail.com>
2020-03-11 16:52:15 +0200

    context example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,52 @@
-import React, { lazy, Suspense }                  from 'react';
+import React, { lazy, Suspense, Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css'
-const SignUpPage = lazy( () => import( './pages/SignUpPage.js' ) );
-const SignInPage = lazy( () => import( './pages/SignInPage.js' ) );
-const HomePage = lazy( () => import( './pages/HomePage.js' ) );
-const TestPage = lazy(()=>import('./pages/TestPage.js'))
+import './App.css';
+
+const SignUpPage = lazy(() => import( './pages/SignUpPage.js' ));
+const SignInPage = lazy(() => import( './pages/SignInPage.js' ));
+const HomePage = lazy(() => import( './pages/HomePage.js' ));
+const TestPage = lazy(() => import('./pages/TestPage.js'));
 const fallbackElem = <div className='loader'>Loading...</div>;
 
-function App () {
+export const ThemeContext = React.createContext('dark');
 
-  return (
-    <Router>
-      <Suspense fallback={fallbackElem}>
-        <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path={['/signup', '/sign_up']} component={SignUpPage}/>
-          <Route path={['/signin', '/sign_in', '/login']} component={SignInPage}/>
-          <Route path={['/test']} component={TestPage}/>
-        </Switch>
-      </Suspense>
-    </Router>
-  );
+class App extends Component {
+
+  constructor (props) {
+    super( props );
+    this.state = {
+      theme: 'light',
+    };
+
+  }
+
+  changeTheme = () => {
+    this.setState( state => ({
+      theme: state.theme === 'light'
+             ? 'dark'
+             : 'light'
+    }) );
+  };
+
+  render () {
+    return (
+      <ThemeContext.Provider value={{
+        changeTheme: this.changeTheme,
+        theme: this.state.theme,
+      }}>
+        <Router>
+          <Suspense fallback={fallbackElem}>
+            <Switch>
+              <Route exact path="/" component={HomePage}/>
+              <Route path={['/signup', '/sign_up']} component={SignUpPage}/>
+              <Route path={['/signin', '/sign_in', '/login']} component={SignInPage}/>
+              <Route path={['/test']} component={TestPage}/>
+            </Switch>
+          </Suspense>
+        </Router>
+      </ThemeContext.Provider>
+    );
+  }
 }
 
 export default App;
