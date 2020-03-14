fd0cc1a2ea9b7af60e81abdcba7f1c5cb9102993
eveins <maxmost512@gmail.com>
2020-03-14 20:02:24 +0200

    minor bugfixes

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -3,5 +3,5 @@ export const THEME_MODE = Object.freeze({
                                           DARK: 'DARK',
                                         });
 
-export const PASSWORD_REGEXP=/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
-export const PASSWORD_TIP='Minimum eight characters, at least one upper case English letter, one lower case English letter, one number and one special character';
+export const PASSWORD_REGEXP=/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/;
+export const PASSWORD_TIP='Minimum eight characters, at least one upper case English letter, one lower case English letter';
